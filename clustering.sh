fcd4039f255f6e100f667509ef1e15e955fa7471
Damien Dosimont <dosimont@users.noreply.github.com>
2015-12-29 12:14:58 +0100

    new scripts to analyze the influence of pct (folding)

--- clustering.sh
+++ clustering.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 
-Copyright (c) 2015 Barcelona Supercomputing Center
-Script to launch the tools of the BSC Performance Tools Team
-and perform a complete analysis process.
+#Copyright (c) 2015 Barcelona Supercomputing Center
+#Script to launch the tools of the BSC Performance Tools Team
+#and perform a complete analysis process.
+#
+#This is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#Author: Damien Dosimont <damien[dot]dosimont[at]bsc.es
 
-This is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-Author: Damien Dosimont <damien[dot]dosimont[at]bsc.es
-
-
-PREFIX=bt.B
+PREFIX=
 SUFFIX=chop1
 CLUSTER=cluster.xml
 
 clustering_fct(){
-  BurstClustering -d $CLUSTER -i ${APP}.prv -o ${APP}.clustered.prv
+  BurstClustering -a -d $CLUSTER -i ${APP}.prv -o ${APP}.clustered.prv
   #sed -i '/pause/d' ${APP}.clustered.IPC.PAPI_TOT_INS.gnuplot
   gnuplot -p ${APP}.clustered.IPC.PAPI_TOT_INS.gnuplot
 }
